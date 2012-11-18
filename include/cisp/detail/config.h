ef9210720a2c224ad285ba8ed410125d2f6435d8
Louis Dionne <louis.dionne92@gmail.com>
2012-11-18 00:16:38 -0500

    Add the config header.

---
+++ include/cisp/detail/config.h
@@ -0,0 +1,45 @@
+/**
+ * Internal configuration file to enable/disable features depending on the
+ * platform.
+ */
+
+#ifndef CISP_DETAIL_CONFIG_H
+#define CISP_DETAIL_CONFIG_H
+
+/**
+ * This flag is on whenever we are being compiled using the C99 dialect.
+ */
+#if !__cplusplus && __STDC_VERSION__ >= 199901L
+#   define CISP_CONFIG_C99 1
+#else
+#   define CISP_CONFIG_C99 0
+#endif
+
+/**
+ * This flag is on whenever we are being compiled using C++11.
+ */
+#if defined(__cplusplus) && __cplusplus >= 201103L
+#   define CISP_CONFIG_CPP11 1
+#else
+#   define CISP_CONFIG_CPP11 0
+#endif
+
+/**
+ * This flag is enabled when it is allowed to pass empty arguments to macros.
+ */
+#if CISP_CONFIG_C99 || CISP_CONFIG_CPP11
+#   define CISP_CONFIG_EMPTY_MACRO_ARGS 1
+#else
+#   define CISP_CONFIG_EMPTY_MACRO_ARGS 0
+#endif
+
+/**
+ * This flag is enabled when variadic macros are supported.
+ */
+#if CISP_CONFIG_C99 || CISP_CONFIG_CPP11
+#   define CISP_CONFIG_VARIADIC_MACROS 1
+#else
+#   define CISP_CONFIG_VARIADIC_MACROS 0
+#endif
+
+#endif /* !CISP_DETAIL_CONFIG_H */
