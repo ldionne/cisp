bed35d4501c4d81afd264c6e4b8e8f750c7e72ae
Louis Dionne <louis.dionne92@gmail.com>
2012-11-17 22:21:26 -0500

    Add basic operations on sequences of fields.

---
+++ include/cisp/detail/type.h
@@ -0,0 +1,285 @@
+/**
+ * Macros to manipulate types, i.e. sequences of fields.
+ */
+
+#ifndef CISP_DETAIL_TYPE_H
+#define CISP_DETAIL_TYPE_H
+
+#include <chaos/preprocessor/seq/core.h>
+
+
+/**
+ * Create an empty sequence of fields.
+ */
+#define CISP_I_EMPTY_TYPE() CHAOS_PP_SEQ_NIL()
+
+/****************************************************************************/
+
+#include <cisp/detail/field.h>
+
+#include <chaos/preprocessor/recursion/expr.h>
+#include <chaos/preprocessor/seq/core.h>
+
+
+/**
+ * Set a new field without checking whether there already exists a field
+ * with the same key.
+ */
+#define CISP_I_SET_FIELD_NO_UPDATE(type, key, value) \
+    CISP_I_SET_FIELD_NO_UPDATE_S(CHAOS_PP_STATE(), type, key, value)
+
+#define CISP_I_SET_FIELD_NO_UPDATE_S(s, type, key, value) \
+    CHAOS_PP_SEQ_CONS(type, CISP_I_FIELD(key, value))
+
+/****************************************************************************/
+
+#include <cisp/detail/field.h>
+
+#include <chaos/preprocessor/control/iif.h>
+#include <chaos/preprocessor/recursion/basic.h>
+#include <chaos/preprocessor/recursion/expr.h>
+#include <chaos/preprocessor/seq/core.h>
+#include <chaos/preprocessor/string/compare.h>
+
+
+/**
+ * Set a field. If there already exists fields with the same key, update the
+ * first of them to be associated with the new value.
+ */
+#define CISP_I_SET_FIELD_UPDATE_FIRST(type, key, value) \
+    CISP_I_SET_FIELD_UPDATE_FIRST_S(CHAOS_PP_STATE(), type, key, value)
+
+#define CISP_I_SET_FIELD_UPDATE_FIRST_S(s, type, key, value)                   \
+    CHAOS_PP_IIF(CHAOS_PP_SEQ_IS_CONS(type)) (                                 \
+                                                                               \
+        CHAOS_PP_EXPR_S(s)(                                                    \
+            CISP_II_SET_FIELD_UPDATE_FIRST(                                    \
+                CHAOS_PP_OBSTRUCT(),                                           \
+                CHAOS_PP_NEXT(s),                                              \
+                CHAOS_PP_SEQ_HEAD(type),                                       \
+                CHAOS_PP_SEQ_TAIL(type),                                       \
+                key,                                                           \
+                value)),                                                       \
+                                                                               \
+        CHAOS_PP_SEQ_CONS(                                                     \
+            CHAOS_PP_SEQ_NIL(),                                                \
+            CISP_I_FIELD(key, value)))                                         \
+/**/
+
+/* Perform a linear search on the sequence of fields. When we find the field,
+ * we replace it and stop. If we don't find it, we append the new field to
+ * the sequence.
+ * Precondition: head is a valid field.
+ */
+#define CISP_II_SET_FIELD_UPDATE_FIRST(_, s, head, tail, key, value)           \
+    CHAOS_PP_IIF _(CHAOS_PP_STRING_COMPARE _(key, CISP_I_FIELD_KEY _(head))) ( \
+        /* If we have found the field, replace it and stop. */                 \
+        CHAOS_PP_SEQ_CONS _(                                                   \
+            tail,                                                              \
+            CISP_I_FIELD(key, value)),                                         \
+                                                                               \
+        CHAOS_PP_IIF _(CHAOS_PP_SEQ_IS_NIL _(tail)) (                          \
+            /* If this is the end of the sequence, append the new field. */    \
+            CHAOS_PP_SEQ_CONS _(                                               \
+                CHAOS_PP_SEQ_CONS _(                                           \
+                    CHAOS_PP_SEQ_NIL(),                                        \
+                    CISP_I_FIELD(key, value)),                                 \
+                head),                                                         \
+            /* Otherwise, call ourselves on the tail, recursively. */          \
+            CHAOS_PP_SEQ_CONS _(                                               \
+                CHAOS_PP_EXPR_S _(s)(CISP_II_SET_FIELD_UPDATE_FIRST_ID _()(    \
+                    CHAOS_PP_OBSTRUCT _(),                                     \
+                    CHAOS_PP_NEXT(s),                                          \
+                    CHAOS_PP_SEQ_HEAD _(tail),                                 \
+                    CHAOS_PP_SEQ_TAIL _(tail),                                 \
+                    key,                                                       \
+                    value)),                                                   \
+                head)))                                                        \
+/**/
+
+#define CISP_II_SET_FIELD_UPDATE_FIRST_ID() CISP_II_SET_FIELD_UPDATE_FIRST
+
+/****************************************************************************/
+
+#include <cisp/detail/field.h>
+
+#include <chaos/preprocessor/control/iif.h>
+#include <chaos/preprocessor/logical/compl.h>
+#include <chaos/preprocessor/recursion/basic.h>
+#include <chaos/preprocessor/recursion/expr.h>
+#include <chaos/preprocessor/seq/core.h>
+#include <chaos/preprocessor/seq/filter.h>
+#include <chaos/preprocessor/string/compare.h>
+
+
+/**
+ * Set a field. If there already exists a field with the same key, the type is
+ * updated so the key appears only once and is associated with the new value.
+ */
+#define CISP_I_SET_FIELD_UPDATE_ALL(type, key, value) \
+    CISP_I_SET_FIELD_UPDATE_ALL_S(CHAOS_PP_STATE(), type, key, value)
+
+#define CISP_I_SET_FIELD_UPDATE_ALL_S(s, type, key, value)                     \
+    CHAOS_PP_IIF(CHAOS_PP_SEQ_IS_CONS(type)) (                                 \
+                                                                               \
+        CHAOS_PP_EXPR_S(s)(                                                    \
+            CISP_II_SET_FIELD_UPDATE_ALL(                                      \
+                CHAOS_PP_OBSTRUCT(),                                           \
+                CHAOS_PP_NEXT(s),                                              \
+                CHAOS_PP_SEQ_HEAD(type),                                       \
+                CHAOS_PP_SEQ_TAIL(type),                                       \
+                key,                                                           \
+                value)),                                                       \
+                                                                               \
+        CHAOS_PP_SEQ_CONS(                                                     \
+            CHAOS_PP_SEQ_NIL(),                                                \
+            CISP_I_FIELD(key, value)))                                         \
+/**/
+
+/* Perform a linear search on the sequence of fields. When we find the field,
+ * we replace it and remove any subsequent field with the same key. If we
+ * don't find it, we prepend the new field to the sequence.
+ * Precondition: head is a valid field.
+ */
+#define CISP_II_SET_FIELD_UPDATE_ALL(_, s, head, tail, key, value)             \
+    CHAOS_PP_IIF _(CHAOS_PP_STRING_COMPARE _(key, CISP_I_FIELD_KEY _(head))) ( \
+        /* If we have found the field, replace it and erase any     */         \
+        /* subsequent field with the same key.                      */         \
+        CHAOS_PP_SEQ_CONS _(                                                   \
+            CHAOS_PP_IIF _(CHAOS_PP_SEQ_IS_NIL _(tail)) (                      \
+                CHAOS_PP_SEQ_NIL(),                                            \
+                CHAOS_PP_EXPR_S _(s)(                                          \
+                    CHAOS_PP_SEQ_FILTER_S _(                                   \
+                        CHAOS_PP_NEXT(s),                                      \
+                        CISP_II_SFU_ALL_CONTINUE_PRED,                         \
+                        tail,                                                  \
+                        key))),                                                \
+            CISP_I_FIELD(key, value)),                                         \
+                                                                               \
+        CHAOS_PP_IIF _(CHAOS_PP_SEQ_IS_NIL _(tail)) (                          \
+            /* If this is the end of the sequence, append the new field. */    \
+            CHAOS_PP_SEQ_CONS _(                                               \
+                CHAOS_PP_SEQ_CONS _(                                           \
+                    CHAOS_PP_SEQ_NIL(),                                        \
+                    CISP_I_FIELD(key, value)),                                 \
+                head),                                                         \
+            /* Otherwise, call ourselves on the tail to continue. */           \
+            CHAOS_PP_SEQ_CONS _(                                               \
+                CHAOS_PP_EXPR_S _(s)(                                          \
+                    CISP_II_SET_FIELD_UPDATE_ALL_ID _()(                       \
+                        CHAOS_PP_OBSTRUCT _(),                                 \
+                        CHAOS_PP_NEXT(s),                                      \
+                        CHAOS_PP_SEQ_HEAD _(tail),                             \
+                        CHAOS_PP_SEQ_TAIL _(tail),                             \
+                        key,                                                   \
+                        value)),                                               \
+                head)))                                                        \
+/**/
+
+#define CISP_II_SET_FIELD_UPDATE_ALL_ID() CISP_II_SET_FIELD_UPDATE_ALL
+
+/* Return whether the field has the same key as key. */
+#define CISP_II_SFU_ALL_CONTINUE_PRED(s, field, key) \
+    CHAOS_PP_COMPL(CHAOS_PP_STRING_COMPARE(key, CISP_I_FIELD_KEY(field)))
+
+/****************************************************************************/
+
+#include <cisp/detail/field.h>
+
+#include <chaos/preprocessor/control/iif.h>
+#include <chaos/preprocessor/recursion/basic.h>
+#include <chaos/preprocessor/recursion/expr.h>
+#include <chaos/preprocessor/seq/core.h>
+#include <chaos/preprocessor/string/compare.h>
+
+
+/**
+ * Remove the first field with a key equal to @p key. Nothing is done if
+ * there is no such field in the type.
+ */
+#define CISP_I_REMOVE_FIRST_FIELD(type, key) \
+    CISP_I_REMOVE_FIRST_FIELD_S(CHAOS_PP_STATE(), type, key)
+
+#define CISP_I_REMOVE_FIRST_FIELD_S(s, type, key)                              \
+    CHAOS_PP_IIF(CHAOS_PP_SEQ_IS_CONS(type)) (                                 \
+        CHAOS_PP_EXPR_S(s)(                                                    \
+            CISP_II_REMOVE_FIRST_FIELD(                                        \
+                CHAOS_PP_OBSTRUCT(),                                           \
+                CHAOS_PP_NEXT(s),                                              \
+                CHAOS_PP_SEQ_HEAD(type),                                       \
+                CHAOS_PP_SEQ_TAIL(type),                                       \
+                key)),                                                         \
+                                                                               \
+        CISP_I_EMPTY_TYPE())                                                   \
+/**/
+
+#define CISP_II_REMOVE_FIRST_FIELD(_, s, head, tail, key)                      \
+    CHAOS_PP_IIF _(CHAOS_PP_STRING_COMPARE _(key, CISP_I_FIELD_KEY _(head))) ( \
+            /* If we have found the field, remove it and stop. */              \
+            tail,                                                              \
+                                                                               \
+            CHAOS_PP_IIF _(CHAOS_PP_SEQ_IS_NIL _(tail)) (                      \
+                /* If the tail is empty, keep the head and stop. */            \
+                CHAOS_PP_SEQ_CONS _(CHAOS_PP_SEQ_NIL(), head),                 \
+                                                                               \
+                /* Otherwise, call ourselves on the tail to continue. */       \
+                CHAOS_PP_SEQ_CONS _(                                           \
+                    CHAOS_PP_EXPR_S _(s)(                                      \
+                        CISP_II_REMOVE_FIRST_FIELD_ID _()(                     \
+                            CHAOS_PP_OBSTRUCT _(),                             \
+                            CHAOS_PP_NEXT(s),                                  \
+                            CHAOS_PP_SEQ_HEAD _(tail),                         \
+                            CHAOS_PP_SEQ_TAIL _(tail),                         \
+                            key)),                                             \
+                    head)))                                                    \
+/**/
+
+#define CISP_II_REMOVE_FIRST_FIELD_ID() CISP_II_REMOVE_FIRST_FIELD
+
+/****************************************************************************/
+
+#include <cisp/detail/field.h>
+
+#include <chaos/preprocessor/control/iif.h>
+#include <chaos/preprocessor/facilities/empty.h>
+#include <chaos/preprocessor/logical/compl.h>
+#include <chaos/preprocessor/recursion/expr.h>
+#include <chaos/preprocessor/seq/core.h>
+#include <chaos/preprocessor/seq/filter.h>
+#include <chaos/preprocessor/string/compare.h>
+
+
+/**
+ * Remove all fields whose key is @p key.
+ */
+#define CISP_I_REMOVE_ALL_FIELDS(type, key) \
+    CISP_I_REMOVE_ALL_FIELDS_S(CHAOS_PP_STATE(), type, key)
+
+#define CISP_I_REMOVE_ALL_FIELDS_S(s, type, key)                               \
+    CHAOS_PP_IIF(CHAOS_PP_SEQ_IS_NIL(type)) (                                  \
+        CISP_I_EMPTY_TYPE(),                                                   \
+                                                                               \
+        CISP_II_REMOVE_ALL_FIELDS(                                             \
+            CHAOS_PP_EXPR_S(s)(                                                \
+                /* this hack is to avoid having an empty macro argument */     \
+                () CHAOS_PP_SEQ_FILTER_S(                                      \
+                    CHAOS_PP_NEXT(s),                                          \
+                    CISP_I_REMOVE_ALL_FIELDS_PRED,                             \
+                    type,                                                      \
+                    key))))                                                    \
+/**/
+
+#define CISP_I_REMOVE_ALL_FIELDS_PRED(s, field, key) \
+    CHAOS_PP_COMPL(CHAOS_PP_STRING_COMPARE(key, CISP_I_FIELD_KEY(field)))
+
+/* Detect whether @p type is empty. We need to eat the leading parenthesis
+ * that we introduced in `CISP_I_REMOVE_ALL_FIELDS` to avoid having an empty
+ * macro argument, which is invalid in C89.
+ */
+#define CISP_II_REMOVE_ALL_FIELDS(type)                                        \
+    CHAOS_PP_IIF(CHAOS_PP_SEQ_IS_NIL(CHAOS_PP_EMPTY type)) (                   \
+        CISP_I_EMPTY_TYPE(),                                                   \
+        CHAOS_PP_EMPTY type)                                                   \
+/**/
+
+#endif /* !CISP_DETAIL_TYPE_H */
---
+++ test/detail/type.c
@@ -0,0 +1,46 @@
+/**
+ * This file contains unit tests for basic type functionalities.
+ */
+
+#include <cisp/detail/type.h>
+
+
+[[[
+
+/* should append field */
+CISP_I_SET_FIELD_UPDATE_FIRST(CISP_I_EMPTY_TYPE(), k e y, value) == ((k e y, value))
+/* should replace the only field */
+CISP_I_SET_FIELD_UPDATE_FIRST(((k e y, value)), k e y, new value) == ((k e y, new value))
+/* should replace first f o o field only */
+CISP_I_SET_FIELD_UPDATE_FIRST(((f o o, foo)) ((b a r, bar)) ((f o o, foo)), f o o, new foo) == ((f o o, new foo)) ((b a r, bar)) ((f o o, foo))
+/* should append the new field */
+CISP_I_SET_FIELD_UPDATE_FIRST(((f o o, foo)) ((b a r, bar)), b a z, baz) == ((f o o, foo)) ((b a r, bar)) ((b a z, baz))
+
+/* should append field */
+CISP_I_SET_FIELD_UPDATE_ALL(CISP_I_EMPTY_TYPE(), k e y, value) == ((k e y, value))
+/* should replace the only field */
+CISP_I_SET_FIELD_UPDATE_ALL(((k e y, value)), k e y, new value) == ((k e y, new value))
+/* should replace first f o o field and remove the other f o o fields */
+CISP_I_SET_FIELD_UPDATE_ALL(((f o o, foo)) ((b a r, bar)) ((f o o, foo)), f o o, new foo) == ((f o o, new foo)) ((b a r, bar))
+/* should append the new field */
+CISP_I_SET_FIELD_UPDATE_ALL(((f o o, foo)) ((b a r, bar)), b a z, baz) == ((f o o, foo)) ((b a r, bar)) ((b a z, baz))
+
+/* should not remove anything from the empty type and therefore yield the empty type */
+CISP_I_REMOVE_FIRST_FIELD(CISP_I_EMPTY_TYPE(), k e y) == CISP_I_EMPTY_TYPE()
+/* should not be able to find the field and therefore should not remove anything */
+CISP_I_REMOVE_FIRST_FIELD(((f o o, foo)) ((b a r, bar)), i n e x i s t a n t  k e y) == ((f o o, foo)) ((b a r, bar))
+/* should remove the only field and yield the empty type */
+CISP_I_REMOVE_FIRST_FIELD(((f o o, foo)), f o o) == CISP_I_EMPTY_TYPE()
+/* should remove only the first matching field */
+CISP_I_REMOVE_FIRST_FIELD(((f o o, foo)) ((b a r, bar)) ((f o o, foo)), f o o) == ((b a r, bar)) ((f o o, foo))
+
+/* should not remove anything from the empty type and therefore yield the empty type */
+CISP_I_REMOVE_ALL_FIELDS(CISP_I_EMPTY_TYPE(), k e y) == CISP_I_EMPTY_TYPE()
+/* should not be able to find the field and therefore should not remove anything */
+CISP_I_REMOVE_ALL_FIELDS(((f o o, foo)) ((b a r, bar)), i n e x i s t a n t  k e y) == ((f o o, foo)) ((b a r, bar))
+/* should remove the only field and yield the empty type */
+CISP_I_REMOVE_ALL_FIELDS(((f o o, foo)), f o o) == CISP_I_EMPTY_TYPE()
+/* should remove all matching fields */
+CISP_I_REMOVE_ALL_FIELDS(((f o o, foo)) ((b a r, bar)) ((f o o, foo)), f o o) == ((b a r, bar))
+
+]]]
